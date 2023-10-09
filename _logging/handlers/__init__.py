d06899c29978eac9fb75cdff5040d65d8b0eb234
Asem-202105001 <as2105001@qu.edu.qa>
2023-10-09 11:21:06 +0300

    .

--- _logging/handlers/__init__.py
+++ _logging/handlers/__init__.py
@@ -10,8 +10,29 @@ def _get_path():
     return join(_project_folder, "log.json")
 
 
-def set_path(path: str):
-    globals()["log_file_path"] = path
+class Path:
+    __log_file_path = _get_path()
+
+    @property
+    def log_file_path(self):
+        return self.__log_file_path
+
+    @log_file_path.setter
+    def log_file_path(self, value):
+        self.__log_file_path = value
 
 
-log_file_path = _get_path()
+__path = Path()
+log_file_path = __path.log_file_path
+
+
+# def get_log_file_path():
+#     return log_file_path
+#
+#
+# def set_path(path: str):
+#     globals()["log_file_path"] = path
+#     print(log_file_path)
+
+
+print(type(log_file_path))
--- tests/main.py
+++ tests/main.py
@@ -1,18 +1,23 @@
-from logging import DEBUG, getLogger
+from logging import getLogger
 from uuid import uuid1
 
-from _logging.configs import brief_console_config, detailed_json_file_config, detailed_console_config
-from _logging.loggers.logger import Logger
-
+from _logging.configs import brief_console_config, detailed_json_file_config
+from _logging.handlers import log_file_path
+log_file_path = "C:\\Users\\asems\\OneDrive\\Desktop\\log.json"
 
 def main():
     logger_name = "test_logger"
 
     # Configuring the logger
+    # globals()["log_file_path"] = "C:\\Users\\asems\\OneDrive\\Desktop\\log.json"
+
     brief_console_config(logger_name)
     detailed_json_file_config(logger_name)
 
+    print(f"Main func: {log_file_path}")
+
     logger = getLogger(logger_name)
+
     logger.debug(f"Test Message from logger.debug #{uuid1}")
     logger.info(f"Test Message from logger.info #{uuid1}")
     logger.warning(f"Test Message from logger.warning #{uuid1}")
